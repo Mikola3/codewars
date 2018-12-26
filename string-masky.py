89d16cb3b0a45b0cd99cb7971f3bd4da78b8210f
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2018-12-26 17:29:17 +0300

    Update string-masky.py

--- string-masky.py
+++ string-masky.py
@@ -15,11 +15,8 @@ def maskify(cc):
             array_new[i]="#"
         return ''.join([str(x) for x in array_new])
 
-#print(maskify("12345"))
-
 print("---Best-Solution---")
 '''
 def maskify(cc):
     return "#"*(len(cc)-4) + cc[-4:]
 '''
-
