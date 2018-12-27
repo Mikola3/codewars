b7756d65faa214ce55010c0518b2e6610fa27df8
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2018-12-27 15:29:40 +0300

    Create get-middle-in-string.py

---
+++ get-middle-in-string.py
@@ -0,0 +1,20 @@
+import math
+def get_middle(s):
+    arr = []
+    new_arr = []
+    for char in s:
+        arr.append(char)
+    if len(arr) % 2 == 0:
+        middle = int(len(arr)/2)
+        new_arr.extend((arr[middle-1], arr[middle]))
+        return ''.join(new_arr)
+    elif len(arr) % 2 == 0:
+        middle = len(s)/2
+        middle_modify = int(math.ceil(middle))
+        return str(arr[middle_modify-1])
+
+print(get_middle("test"))
+print(get_middle("testing"))
+print(get_middle("middle"))
+print(get_middle("A"))
+print(get_middle("of"))
