9ad87f0ac4c7237e3c53b5f46b91a31a983653b9
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2019-01-04 13:44:56 +0300

    Update number-multiplicative-persistence.py

--- junior/number-multiplicative-persistence.py
+++ junior/number-multiplicative-persistence.py
@@ -28,3 +28,38 @@ def persistence(num):
     return count
 
 #print(persistence(39)) == 3
+
+
+print("----Better-Solusion------")
+'''
+import operator
+def persistence(n):
+    i = 0
+    while n>=10:
+        n=reduce(operator.mul,[int(x) for x in str(n)],1)
+        i+=1
+    return i
+#print(operator.mul(3,9)) == 27
+'''
+'''
+def persistence(n):
+    nums = [int(x) for x in str(n)]
+    sist = 0
+    while len(nums) > 1:
+        newNum = reduce(lambda x, y: x * y, nums)
+        nums = [int(x) for x in str(newNum)]
+        sist = sist + 1
+    return sist
+'''
+'''
+def persistence(n):
+    n = str(n)
+    count = 0
+    while len(n) > 1:
+        p = 1
+        for i in n:
+            p *= int(i)
+        n = str(p)
+        count += 1
+    return count
+'''
