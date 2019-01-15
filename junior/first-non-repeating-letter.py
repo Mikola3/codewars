58089ea1cefd0d24e0a376b8bd30e3c2f283d68f
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2019-01-15 19:55:08 +0300

    Update first-non-repeating-letter.py

--- junior/first-non-repeating-letter.py
+++ junior/first-non-repeating-letter.py
@@ -12,3 +12,23 @@ def first_non_repeating_letter(string):
             for i in [i for i,x in enumerate(new_string) if x == letter]: # i is a position non-repeating letter in the new string
                 return string[i] # non-repeating letter in the initial string
     return ''
+
+print("---Beteer-Solution---")
+'''
+def first_non_repeating_letter(string):
+    string_lower = string.lower()
+    for i, letter in enumerate(string_lower):
+        if string_lower.count(letter) == 1:
+            return string[i]
+            
+    return ""
+'''
+'''
+from collections import Counter
+def first_non_repeating_letter(string):
+    cnt = Counter(string.lower())
+    for letter in string:
+        if cnt[letter.lower()] == 1:
+            return letter
+    return ''
+'''
